db43759250f100651f014ddd67d703e9113524cc
IIfdbi <dyqdyqyq46550@163.com>
2021-03-23 15:47:46 +0800

    update

---
+++ src/main/java/find_friend/repository/NeedRepository.java
@@ -0,0 +1,12 @@
+package find_friend.repository;
+
+import find_friend.po.Need;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by mengwei on 2018/4/12.
+ */
+public interface NeedRepository extends JpaRepository<Need, String>{
+
+
+}
